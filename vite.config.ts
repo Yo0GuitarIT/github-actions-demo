e5faabf1e1c899eb4fbabe68e939978ea5e507dc
Yu Ling Chen <chenYuLing@hismax.onmicrosoft.com>
2025-06-13 19:03:55 +0800

    Fix CI/CD workflow and update Vite config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-const basePath = 'https://github.com/Yo0GuitarIT/github-actions-demo' 
+const basePath = "/github-actions-demo/";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: basePath,
   plugins: [react()],
-})
+});

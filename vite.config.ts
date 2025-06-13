0c22d7e14dde48314d610d653c311c06a1e78d7d
Yu Ling Chen <chenYuLing@hismax.onmicrosoft.com>
2025-06-13 18:56:51 +0800

    Add CI/CD workflow for GitHub Pages deployment and update package.json for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const basePath = 'https://github.com/Yo0GuitarIT/github-actions-demo' 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: basePath,
   plugins: [react()],
 })
